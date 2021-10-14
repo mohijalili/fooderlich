b797351c81de23d41441b16af48a46b656ce6f29
Mohammad Jalili <mohijalili@icloud.com>
2021-10-14 14:12:35 +0800

    feat(app): initial app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create theme
+    // TODO: Apply Home widget
+    return MaterialApp(
+      // TODO: Add theme
+      title: 'Fooderlich',
+      home: Scaffold(
+        // TODO: Style the title
+        appBar: AppBar(
+          title: const Text('Fooderlich'),
+        ),
+        // TODO: Style the body text
+        body: const Center(child: Text('Let\'s get cooking 👩‍🍳')),
+      ),
+    );
+  }
+}
